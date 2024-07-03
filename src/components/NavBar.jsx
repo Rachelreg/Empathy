cfe791e77e4d1ca85d542952a96f4620c73a5e15
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-07-03 18:14:42 +0200

    Addinf the link to logo and correction for code without div

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,22 @@
+import { Link } from "react-router-dom";
 import logoProfil from "../assets/icon_profil.png";
 import logoSite from "../assets/logo_site.png"
 import "../styles/NavBar.css";
 
 export default function NavBar() {
   return (
-    <section className="container_navbar">
-      <div className="logo_profil">
+    <nav className="container_navbar">
+      <header className="logo_profil">
+        <Link to="/">
         <img src={logoProfil} alt="logo profil" />
-      </div>
-      <div className="logo_site">
+        </Link>
+      </header>
+      <figure className="logo_site">
         <img src={logoSite} alt="logo du site" />
-      </div>
-      <div className="langue_fr">
+      </figure>
+      <section className="langue_fr">
         <p>FR</p>
-      </div>
-    </section>
+      </section>
+    </nav>
   );
 }
