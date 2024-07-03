41362623d44bf88d46cf6a44bad922374c687132
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-07-03 16:10:35 +0200

    JSX ok without css

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,8 +1,11 @@
+import Confiance from "../components/Confiance";
+
 export default function Accueil() {
   return (
     <>
       <h1>Accueil</h1>
       <p>toto</p>
+      <Confiance />
     </>
   );
 }
