5c99905a25ad35944732fe9207c513d6d186d653
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-07-03 17:13:31 +0200

    Rajout de mon composant

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,10 +1,12 @@
 
+import Confiance from "../components/Confiance";
 import Entete from "../components/Entete";
 
 export default function Accueil() {
   return (
     <>    
       <Entete/>
+      <Confiance/>
     </>
   );
 }
