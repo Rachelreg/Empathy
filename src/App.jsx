e18597f967d12700f111b560d6a912cad7f5a733
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-07-03 16:14:47 +0200

    based navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import "./styles/App.css"
 
 function App() {
   return (
     <>
+      <NavBar/>
       <Outlet />
       <Footer />
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,17 @@
+import logoProfil from "../assets/icon_profil.png";
+import "../styles/NavBar.css";
+
 export default function NavBar() {
   return (
     <section className="container_navbar">
       <div className="logo_profil">
-        <img src="" alt="" />
+        <img src={logoProfil} alt="logo profil" />
       </div>
       <div className="logo_site">
-        <img src="" alt="" />
+        <h1>LOGO</h1>
       </div>
       <div className="langue_fr">
-        <img src="" alt="" />
+        <p>FR</p>
       </div>
     </section>
   );
