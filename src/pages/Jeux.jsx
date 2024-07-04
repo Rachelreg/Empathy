de57609003f72f997475cda284e99178a490bb1a
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-07-04 15:50:33 +0200

    Change jeux

--- src/pages/Jeux.jsx
+++ src/pages/Jeux.jsx
@@ -1,10 +1,21 @@
-import Question from "../components/Question.jsx";
+import { Link } from "react-router-dom";
 import "../styles/Jeux.css"
 
 export default function Jeux() {
   return (
-    <section className="container_games">
-      <Question />
+    <section className="container_games3">
+        <section className="p_jeux">
+            <p>Allez à la rencontre de vos collègues en cliquant dans une salle</p>
+        </section>
+        <Link to="/beforequest1">
+<button className="button1_game"></button>
+        </Link>
+<button className="button2_game"></button>
+<button className="button3_game"></button>
+<button className="button4_game"></button>
+<button className="button5_game"></button>
+<button className="button6_game"></button>
+<button className="button7_game"></button>
     </section>
   );
 }
