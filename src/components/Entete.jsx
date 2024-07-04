00b4d20a0e08f2339d50ea471cb6272906a6987f
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-07-04 12:23:39 +0200

    Finish page Accueil

--- src/components/Entete.jsx
+++ src/components/Entete.jsx
@@ -5,7 +5,7 @@ export default function Entete() {
 
   return (
     <section className="container_entete">
-      <h1>SERIOUS GAME</h1>
+      <h1>BIENVENUE</h1>
       <p>Empathy a été créé pour <span>sensibiliser</span> les employés aux enjeux de l'inclusivité en entreprise, leur faire comprendre les bonnes pratiques et les comportements appropriés pour favoriser <span>un environnement inclusif</span>. Le jeu se déroule dans un environnement de bureau virtuel où les joueurs naviguent à travers différents pièces et répondent à des questions ou à des scénarios liés à l'inclusivité.</p>
       <Link to="/jeuxdebut">
       <button className="button_game">Commencer la démo</button>
