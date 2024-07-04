cdc0479b1415d6007babaa818e32896a8e41ef46
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-07-04 12:03:26 +0200

    Part accroche ok with css

--- src/components/Accroche.jsx
+++ src/components/Accroche.jsx
@@ -5,8 +5,9 @@ import Fauteuil from "../assets/fauteuil_image.png";
 export default function Accroche() {
   return (
     <section className="box-accroche">
+      <h1>Changez le monde avec empathie : jouez, apprenez, incluez !</h1>
       <section className="woman-box">
-        <img src={WomenPregnant} alt="Avatar de femme enceinte"/>
+        <img src={WomenPregnant} alt="Avatar de femme enceinte" />
         <p>
           Plongez dans notre nouveau serious game signé Empathy, une expérience
           immersive dédiée à l'inclusion ! L'information est la première étape
@@ -17,7 +18,7 @@ export default function Accroche() {
         </p>
       </section>
       <section className="men-box">
-      <img src={Fauteuil} alt="Avatar homme en fauteuil roulant"/>
+        <img src={Fauteuil} alt="Avatar homme en fauteuil roulant" />
         <p>
           Apprenez à identifier et surmonter les obstacles, et semez des graines
           de changement positif autour de vous. Saisissez cette opportunité de
