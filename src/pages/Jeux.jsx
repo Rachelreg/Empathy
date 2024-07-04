d6e8db76112fd857316a1076ec3e4b159eaefe1b
unknown <sebastien.creuze@live.fr>
2024-07-04 11:03:06 +0200

    finish bad quest

--- src/pages/Jeux.jsx
+++ src/pages/Jeux.jsx
@@ -1,18 +1,24 @@
-import "../styles/Jeux.css"
+import BadQuest2 from "../components/BadQuest2";
+import "../styles/Jeux.css";
 
 export default function Jeux() {
   return (
-    <section className="container_games">
+    <>
+      <section className="container_games">
         <section className="p_jeux">
-            <p>Allez à la rencontre de vos collègues en vous rendant dans une salle</p>
+          <p>
+            Allez à la rencontre de vos collègues en vous rendant dans une salle
+          </p>
         </section>
-<button className="button1_game"></button>
-<button className="button2_game"></button>
-<button className="button3_game"></button>
-<button className="button4_game"></button>
-<button className="button5_game"></button>
-<button className="button6_game"></button>
-<button className="button7_game"></button>
-    </section>
+        <button className="button1_game"></button>
+        <button className="button2_game"></button>
+        <button className="button3_game"></button>
+        <button className="button4_game"></button>
+        <button className="button5_game"></button>
+        <button className="button6_game"></button>
+        <button className="button7_game"></button>
+        <BadQuest2 />
+      </section>
+    </>
   );
 }
